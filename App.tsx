a991140fdd01ee3093fe640c456a1dc07ebf629b
Yinon Cohen <yinonc@wix.com>
2020-03-27 17:28:40 +0300

    run prettier

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Noam, Lior, Oriel and Yinon!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Hello Noam, Lior, Oriel and Yinon!</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
--- schemas.d.ts
+++ schemas.d.ts
@@ -1,38 +1,38 @@
-type GameType = 'soccer' | 'basketball' | 'tennis' | 'running' | 'cycling';
+type GameType = 'soccer' | 'basketball' | 'tennis' | 'running' | 'cycling'
 
 interface Game {
-    type: GameType;
-    icon: string;
-    displayName: string; // Maybe we can use GameType as display name?
+    type: GameType
+    icon: string
+    displayName: string // Maybe we can use GameType as display name?
 }
 
 interface User {
-    id: string; // Unique key
-    firstName: string;
-    lastName: string;
-    nickName: string;
-    age: Date;
-    rate: number;
-    profileImage: string; // should be URL / URI
-    favoriteGame: Game;
-    email?: string;
-    friends: User[];
+    id: string // Unique key
+    firstName: string
+    lastName: string
+    nickName: string
+    age: Date
+    rate: number
+    profileImage: string // should be URL / URI
+    favoriteGame: Game
+    email?: string
+    friends: User[]
 }
 
-type participantType = 'GroupManager' | 'Regular';
+type participantType = 'GroupManager' | 'Regular'
 
 interface Group {
     users: {
-        [userId: string]: participantType;
-    }; // In this way we can save both the 'participantType' and to get user in group with o(1) complexity.
-    private: boolean;
-    events: SportEvent[];
+        [userId: string]: participantType
+    } // In this way we can save both the 'participantType' and to get user in group with o(1) complexity.
+    private: boolean
+    events: SportEvent[]
 }
 
 interface SportEvent {
-    users: User[];
-    game: Game;
-    maxUsers: number;
-    location: string; // Google place id / something else ?
-    date: Date;
+    users: User[]
+    game: Game
+    maxUsers: number
+    location: string // Google place id / something else ?
+    date: Date
 }
