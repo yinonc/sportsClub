b946b76d91fdbf23c20d8bfde169db86444ecb11
Yinon Cohen <yinonc@wix.com>
2020-09-20 16:27:22 +0300

    fixing types

--- schemas.d.ts
+++ schemas.d.ts
@@ -6,41 +6,39 @@ export type GameType =
     | 'cycling'
     | 'baseball'
 
+type userId = string
+
 export interface Game {
     type: GameType
     icon: string
-    displayName: string // Maybe we can use GameType as display name?
 }
 
 export interface UserData {
-    id: string // Unique key
+    id: userId
     firstName: string
     lastName: string
     nickName: string
     dateOfBirth: Date
-    rate: number
-    profileImage: string // should be URL / URI
+    rate: number // Think about it
+    profileImage: string
     favoriteGames: GameType[]
     email?: string
-    friends: User[]
+    friends: userId[]
     gamesPlayed: number
 }
 
-export type participantType = 'GroupManager' | 'Regular'
-
 export interface Group {
     id: string
-    users: {
-        [userId: string]: participantType
-    } // In this way we can save both the 'participantType' and to get user in group with o(1) complexity.
+    users: userId[]
     private: boolean
     events: SportEvent[]
+    admins: userId[]
 }
 
 export interface SportEvent {
     groupId?: Group['id']
     eventId: string
-    userIds: UserData['id'][]
+    userIds: userId[]
     gameType: GameType
     title: string
     maxUsers: number
